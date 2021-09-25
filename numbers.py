3180ee4f157ed48ddfc5a740ec88bc375f9ae7c7
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2021-09-25 16:56:28 -0300

    fixed indentation errors

--- numbers.py
+++ numbers.py
@@ -3,34 +3,33 @@ import random
 class Numbers:
 
     def __init__(self, w, h, checker):
-	self.number = 0
-	self.top = 9
-	self.w = w
-	self.h = h
-	self.x = self.y = -1
-	self.checker = checker
-	self.valid = False
+        self.number = 0
+        self.top = 9
+        self.w = w
+        self.h = h
+        self.x = self.y = -1
+        self.checker = checker
+        self.valid = False
 
     def next(self):
+        if self.number == self.top:
+            # option: don't show any more numbers
+            #self.valid = False
+            #return False
 
-	if self.number == self.top:
-	    # option: don't show any more numbers
-	    #self.valid = False
-	    #return False
+            # option: start over at 1
+            self.number = 0
 
-	    # option: start over at 1
-	    self.number = 0
-
-	self.number += 1
-	self.rand_pos()
-	while not self.checker.check_space_empty(self.x, self.y):
-	    self.rand_pos()
-	self.valid = True
-	return True
+        self.number += 1
+        self.rand_pos()
+        while not self.checker.check_space_empty(self.x, self.y):
+            self.rand_pos()
+        self.valid = True
+        return True
 
     def rand_pos(self):
-	self.x = random.randint(0, self.w - 1)
-	self.y = random.randint(0, self.h - 1)
+        self.x = random.randint(0, self.w - 1)
+        self.y = random.randint(0, self.h - 1)
 
     def get_x(self): return self.x
     def get_y(self): return self.y
